a16bcf8fbd19f93bcffc7b8556d6446b914a3391
shschmahl42 <Sam.Schmahl@gmail.com>
2025-01-29 12:21:14 -0600

    Update to problem 1251

--- sql_50.sql
+++ sql_50.sql
@@ -106,7 +106,7 @@ ORDER BY id DESC;
 SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units), 2), 0) AS average_price 
 FROM Prices p 
 LEFT JOIN UnitsSold u ON p.product_id = u.product_id 
-WHERE u.purchase_date BETWEEN p.start_date AND p.end_date 
+AND u.purchase_date BETWEEN p.start_date AND p.end_date 
 GROUP BY product_id;
 
 -- 1075: Project Employyes I
